38d2c19b036625645aa9501bb7b9465ce8c62b1c
Anthony Nandaa <profnandaa@gmail.com>
2020-02-25 01:06:25 +0300

    feat: invert tree challenge [wip] (#1)
    * wip: created a basic BST
    
    * wip: minor refactors
    
    * wip: refactor to recursive insert
    
    * wip: clean-up

--- samples/invert_tree.js
+++ samples/invert_tree.js
@@ -1,52 +1,4 @@
-class Node {
-    constructor(key) {
-        this.key = key
-        this.left = null
-        this.right = null
-    }
-}
-
-//   root
-//    |
-//    v 
-// [ 10, 5, 3, 20, 6, 8 ]
-class Tree {
-    // we will construct a non-balanced
-    // tree
-    constructor(arr) {
-        // create the root
-        if (!(Array.isArray(arr) && arr.length > 0)) {
-            this.root = null
-            return
-        }
-        for (let i = 1; i < arr.length; i++) {
-            this.addNode(arr[i]);
-        }
-    }
-
-    addNode(key)
-    {
-        let node = new Node(key);
-        // ...
-        // use getInsertionPoint
-    }
-
-    traverse() {
-        // prints out the tree
-        // in order, starting
-        // from the root
-    }
-
-    getInsertionPoint(key) {
-        // traverses the tree upto the
-        // the node where you can add
-        // the key, in order
-        // returns the node
-
-        // start with this.root
-        // add traverse to the right place
-    }
-}
+const { Tree } = require('./commons/BST')
 
 function swap(node1, node2) {
     const temp = node1
@@ -67,11 +19,9 @@ function invertTree(node) {
 }
 
 // tests
-let arr = [10, 5, 20, 4, 3, 12 ]
+let arr = [5, 20, 5, 3, 4, 10, 12, 2, 9, 25 ]
 let t = new Tree(arr)
-invertTree(t)
+t.insertNode(23)
+t.insertNode(30)
+// invertTree(t.root)
 t.traverse()
-
-
-// TODO: check out the actual interface
-// for Tree ADT (abstract data type)
