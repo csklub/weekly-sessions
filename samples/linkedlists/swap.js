fc80aeffbea776dfdeb0283ccaa295b0e596edb7
Anthony Nandaa <profnandaa@gmail.com>
2020-03-28 17:51:36 +0300

    chore: add more explanation for #7 solution

--- samples/linkedlists/swap.js
+++ samples/linkedlists/swap.js
@@ -13,6 +13,33 @@ you should return the list as 2->1->4->3.
 const LinkedList = require("../commons/js/LinkedList");
 
 // fresh take
+/*
+
+starting position:
+
+prev  cur
+  |    |
+  V    V
+  1 -> 2 -> 3 -> 4 -> null
+
+then cur and prev keeps skipping
+2 steps after swap is done.
+
+so the next position is:
+
+    prevCur prev cur
+       |    |    |
+       V    V    V
+  2 -> 1 -> 3 -> 4 -> null
+
+and so forth...
+
+However, the swap also swaps up
+prev and cur, and therefore we have
+to recalibrate/restore them; 
+hence the "recalibrate" section in the code.
+
+*/
 
 function swapLinkedList(list) {
     function _swap(prevCur, prev, cur) {
@@ -46,12 +73,28 @@ function swapLinkedList(list) {
 }
 
 // test
-let l = new LinkedList();
-
-for (let elem of [1, 2, 4, 7, 10]) {
-    l.append(elem);
+// quick helper function
+function listFromArray(arr) {
+    let list = new LinkedList();
+    for (let elem of arr) {
+        list.append(elem);
+    }
+    return list;
 }
+
+// test case 1
+let l = listFromArray([1, 2, 4, 7, 10]);
 
 console.log(l._toArray()); // [ 1, 2, 4, 7, 10 ]
 swapLinkedList(l);
 console.log(l._toArray()); // [ 2, 1, 7, 4, 10 ]
+
+// test case 2
+l = listFromArray([1, 2, 3, 4]);
+swapLinkedList(l);
+console.log(l._toArray()); // [ 2, 1, 4, 3 ]
+
+// test case 3
+l = listFromArray([1]);
+swapLinkedList(l);
+console.log(l._toArray()); // [ 1 ]
