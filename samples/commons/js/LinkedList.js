1a7bc759f182a40f2a4ab973b7a3ab95b2024b71
me-x-mi <ezrqnkemboi@gmail.com>
2020-03-28 18:09:59 +0300

    ft: add more linked list methods - insertAt(value, index) - removeAt(index) - isEmpty() - size()

--- samples/commons/js/LinkedList.js
+++ samples/commons/js/LinkedList.js
@@ -9,6 +9,10 @@ popFirst()            -> Remove a node from the beginning
 head()                -> Return the first node
 tail()                -> Return the last node
 remove(Node)*         -> Remove Node from the list
+size()                -> Return the length/size of the list
+isEmpty()             -> Check if a list is Empty
+insertAt(value, index) -> Add a node at a given index
+removeAt(index)       -> Remove a node at a given index
 */
 
 // NOTE: no type-safety
@@ -24,6 +28,8 @@ class LinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
+        // initiate size of list to 0
+        this.sizeOfList = 0;
     }
 
     append(value) {
@@ -37,12 +43,14 @@ class LinkedList {
             this.tail.next = node;
             this.tail = node;
         }
+        this.sizeOfList++;
     }
 
     prepend(value) {
         let node = new Node(value);
         node.next = this.head;
         this.head = node;
+        this.sizeOfList++;
     }
 
     pop() {
@@ -56,10 +64,11 @@ class LinkedList {
         // move till the 2nd last
         while (it.next.next)
             it = it.next;
-        
+
         let last = this.tail;
         this.tail = it;
         this.tail.next = null;
+        this.sizeOfList--;
         return last;
     }
 
@@ -70,7 +79,55 @@ class LinkedList {
             first.next = null;
         }
         else this.head = null;
+        this.sizeOfList--;
         return first;
+    }
+
+    insertAt(value, index) {
+        if (index > 0 && index > this.sizeOfList) {
+            return false;
+        } else {
+            const node = new Node(value);
+            let curr, prev;
+            curr = this.head;
+            if (index === 0) {
+                node.next = this.head;
+                this.head = node;
+            } else {
+                curr = this.head;
+                let initialIndex = 0;
+                while (initialIndex < index) {
+                    initialIndex++;
+                    prev = curr;
+                    curr = curr.next;
+                }
+                node.next = curr;
+                prev.next = node;
+            }
+            this.sizeOfList++;
+        }
+    }
+
+    removeAt(index) {
+        if (index > 0 && index > this.sizeOfList) {
+            return false;
+        } else {
+            let curr, prev;
+            curr = this.head;
+            prev = curr;
+            if (index === 0) {
+                this.head = curr.next;
+            } else {
+                let initialIndex = 0;
+                while (initialIndex < index) {
+                    initialIndex++;
+                    prev = curr;
+                    curr = curr.next;
+                }
+                prev.next = curr.next;
+            }
+            this.sizeOfList--;
+        }
     }
 
     head() {
@@ -79,6 +136,14 @@ class LinkedList {
 
     tail() {
         return this.tail;
+    }
+
+    size() {
+        return this.sizeOfList;
+    }
+
+    isEmpty() {
+        return this.sizeOfList === 0;
     }
 
     _toArray() {
