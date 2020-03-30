2dae0677a0e6a27a2de86e7f58f5118de8cf6075
Ezrqn Kemboi <ezrqnkemboi@gmail.com>
2020-03-30 17:35:41 +0300

    ft: add more linked list methods (#8)
    * ft: add more linked list methods
    - insertAt(value, index)
    - removeAt(index)
    - isEmpty()
    - size()
    
    * fix: insertAt(value, index) in linked list

--- samples/commons/js/LinkedList.js
+++ samples/commons/js/LinkedList.js
@@ -9,6 +9,10 @@ popFirst()            -> Remove a node from the beginning
 head()                -> Return the first node
 tail()                -> Return the last node
 remove(Node)*         -> Remove Node from the list
+size()                -> Return the length/size of the list
+isEmpty()             -> Check if a list is Empty
+insertAt(value, index) -> Add a node at a given index
+removeAt(index)       -> Remove a node at a given index
 */
 
 // NOTE: no type-safety
@@ -24,6 +28,8 @@ class LinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
+        // initiate size of list to 0
+        this._size = 0;
     }
 
     append(value) {
@@ -37,12 +43,14 @@ class LinkedList {
             this.tail.next = node;
             this.tail = node;
         }
+        this._size++;
     }
 
     prepend(value) {
         let node = new Node(value);
         node.next = this.head;
         this.head = node;
+        this._size++;
     }
 
     pop() {
@@ -56,10 +64,11 @@ class LinkedList {
         // move till the 2nd last
         while (it.next.next)
             it = it.next;
-        
+
         let last = this.tail;
         this.tail = it;
         this.tail.next = null;
+        this._size--;
         return last;
     }
 
@@ -70,7 +79,64 @@ class LinkedList {
             first.next = null;
         }
         else this.head = null;
+        this._size--;
         return first;
+    }
+
+    insertAt(value, index) {
+        if (index < 0 || index > this._size) {
+            return false;
+        } else {
+            const node = new Node(value);
+            if (index === this._size) {
+                /**
+                 * we could use .append() -> but this is more clarifying
+                 * NB: if .append() is used, then, this._size++, 
+                 * should be inside prev {} -> since .append() has its this._size++
+                 */
+                if (!this.head) {
+                    this.head = node;
+                    this.tail = node;
+                } else {
+                    this.tail.next = node;
+                    this.tail = node;
+                }
+            } else {
+                let curr, prev;
+                curr = this.head;
+                let initialIndex = 0;
+                while (initialIndex < index) {
+                    initialIndex++;
+                    prev = curr;
+                    curr = curr.next;
+                }
+                node.next = curr;
+                prev.next = node;
+            }
+            this._size++;
+        }
+    }
+
+    removeAt(index) {
+        if (index < 0 && index > this._size) {
+            return false;
+        } else {
+            let curr, prev;
+            curr = this.head;
+            prev = curr;
+            if (index === 0) {
+                this.head = curr.next;
+            } else {
+                let initialIndex = 0;
+                while (initialIndex < index) {
+                    initialIndex++;
+                    prev = curr;
+                    curr = curr.next;
+                }
+                prev.next = curr.next;
+            }
+            this._size--;
+        }
     }
 
     head() {
@@ -79,6 +145,14 @@ class LinkedList {
 
     tail() {
         return this.tail;
+    }
+
+    size() {
+        return this._size;
+    }
+
+    isEmpty() {
+        return this._size === 0;
     }
 
     _toArray() {
