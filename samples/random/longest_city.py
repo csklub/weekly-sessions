50998cd7de011ce7d413f3da6caad164b042896d
Anthony Nandaa <annandaa@microsoft.com>
2020-04-25 20:19:51 +0300

    session w/ ed: optimized version

--- samples/random/longest_city.py
+++ samples/random/longest_city.py
@@ -1,13 +1,25 @@
 
 
 def longest_name(cities):
-    lens = [ len(c) for c in cities ] # list comprehension
+    # lens = [ len(c) for c in cities ] # list comprehension
     # [ <evalutation> for <item> in <another_list> ]
     # longest = max(lens)
     # index = lens.index(longest)
     # return cities[index]
-    # short version
-    return cities[lens.index(max(lens))]
+    # short version -> not optimized // O(cN) time, O(N) in space
+    # return cities[lens.index(max(lens))]
+    # optimized version -> O(N)
+    longest = 0
+    index = 0
+    i = 0
+    for c in cities:
+        len_ = len(c)
+        if len_ > longest:
+            longest = len_
+            index = i
+        i += 1
+    
+    return cities[index]
 
 
 # test
